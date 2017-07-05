1f26e36d62ebf10c2dedbeb18ca9ef1c71d639ba
Nícolas Cruz <nicolasalves.cruz@gmail.com>
2017-07-05 18:43:57 -0400

    Some Updates

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -3,7 +3,7 @@ app.controller("mainCtrl", function ($scope,$localStorage) {
   // Valores salvos no navegador
   $scope.bodyClass = $localStorage.bodyClass || "no-contrast";
 
-  // Alterna contraste 
+  // Alterna contraste
   $scope.toggleContrast = function(){
     if($scope.bodyClass == "no-contrast"){
       $localStorage.bodyClass = "contrast"
