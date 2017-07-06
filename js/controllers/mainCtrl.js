89b5421422e257d10498d3a0fa5b1a407ad892f2
Nícolas Cruz <nicolasalves.cruz@gmail.com>
2017-07-05 22:19:12 -0400

    Minified some JS

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,16 +1,3 @@
-app.controller("mainCtrl", function ($scope,$localStorage) {
-
-  // Valores salvos no navegador
-  $scope.bodyClass = $localStorage.bodyClass || "no-contrast";
-
-  // Alterna contraste
-  $scope.toggleContrast = function(){
-    if($scope.bodyClass == "no-contrast"){
-      $localStorage.bodyClass = "contrast"
-      $scope.bodyClass = "contrast"
-    }else{
-      $localStorage.bodyClass = "no-contrast"
-      $scope.bodyClass = "no-contrast"
-    }
-  }
-});
+app.controller("mainCtrl",function($scope,$localStorage){$scope.bodyClass=$localStorage.bodyClass||"no-contrast";$scope.toggleContrast=function(){if($scope.bodyClass=="no-contrast"){$localStorage.bodyClass="contrast"
+$scope.bodyClass="contrast"}else{$localStorage.bodyClass="no-contrast"
+$scope.bodyClass="no-contrast"}}});
